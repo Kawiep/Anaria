4546b284634be6e3c2738c60c8570d3eafff678a
Kawiep <38072165+Kawiep@users.noreply.github.com>
2019-04-09 10:29:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ bot.on("ready", () => {
   bot.user.setGame(`>_ Anaria V1.0.0 by Kaw!ep™`, "https://www.twitch.tv/AriaV1")
 });
 
-bot.login('NTY0MTIwODI4OTU4OTk4NTQy.XKjRlg.vAyhYPMVu8cfYnPwCQH_pw-uLGE')
+bot.login('process.env.TOKEN')
 
 bot.on('message', message => {
   
@@ -68,3 +68,92 @@ bot.on('guildMemberAdd', member => {
       message.reply('Je suis connecté sur' + " " + bot.guilds.size + " " + 'Serveurs !');
     }
   });
+
+  
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // If the message content starts with "!kick"
+  if (message.content.startsWith('-kick')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Kick the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         */
+        member.kick('raison optionnelle qui s\'affichera dans les journaux d\'audit').then(() => {
+          // We let the message author know we were able to kick the person
+          message.reply(`**${user.tag}** A bien été Kick !`);
+        }).catch(err => {
+          // An error happened
+          // This is generally due to the bot not being able to kick the member,
+          // either due to missing permissions or role hierarchy
+          message.reply('Je ne peux pas kick ce joueur');
+          // Log the error
+          console.error(err);
+        });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply('Ce joueur n\'est pas sur ce discord !');
+      }
+    // Otherwise, if no user was mentioned
+    } else {
+      message.reply('Tu n\'as pas mentionné le joueur à Kick !');
+    }
+  }
+});
+
+client.on('message', message => {
+  // Ignore messages that aren't from a guild
+  if (!message.guild) return;
+
+  // if the message content starts with "!ban"
+  if (message.content.startsWith('-ban')) {
+    // Assuming we mention someone in the message, this will return the user
+    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
+    const user = message.mentions.users.first();
+    // If we have a user mentioned
+    if (user) {
+      // Now we get the member from the user
+      const member = message.guild.member(user);
+      // If the member is in the guild
+      if (member) {
+        /**
+         * Ban the member
+         * Make sure you run this on a member, not a user!
+         * There are big differences between a user and a member
+         * Read more about what ban options there are over at
+         * https://discord.js.org/#/docs/main/stable/class/GuildMember?scrollTo=ban
+         */
+        member.ban({
+          reason: 'n\'a pas respecté le reglement !',
+        }).then(() => {
+          // We let the message author know we were able to ban the person
+          message.reply(`**${user.tag}** A bien été Ban !`);
+        }).catch(err => {
+          // An error happened
+          // This is generally due to the bot not being able to ban the member,
+          // either due to missing permissions or role hierarchy
+          message.reply('Je ne peux pas Ban ce joueur');
+          // Log the error
+          console.error(err);
+        });
+      } else {
+        // The mentioned user isn't in this guild
+        message.reply('Ce joueur n\'est pas sur ce discord !');
+      }
+    } else {
+    // Otherwise, if no user was mentioned
+      message.reply('Tu n\'as pas mentionné le joueur à Ban !');
+    }
+  }
+});
